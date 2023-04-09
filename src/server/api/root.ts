cf0d84d29ad098a90244d70e467cbddeac907be4
Sriharsh Amur <sriharshamur@gmail.com>
2023-04-09 16:32:40 +0200

    Added Page Layout, profile endpoint and profile getStaticProps

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { postsRouter } from "./routers/posts";
+import { profileRouter } from "./routers/profile";
 
 /**
  * This is the primary router for your server.
@@ -9,6 +10,7 @@ import { postsRouter } from "./routers/posts";
 export const appRouter = createTRPCRouter({
   // example: exampleRouter,
   posts: postsRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
